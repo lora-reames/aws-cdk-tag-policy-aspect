ed3752b7b7d2b66cec4dff82280b5f5bed7c59d0
Lora Reames <shes.lora.reames@gmail.com>
2025-02-08 20:01:00 -0600

    upgrade to latest cdk lib

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,13 +2,14 @@ import { awscdk, javascript } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Lora Reames',
   authorAddress: 'shes.lora.reames@gmail.com',
-  cdkVersion: '2.68.0',
+  cdkVersion: '2.178.1',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.7.0',
   name: 'aws-cdk-tag-policy-aspect',
   packageManager: javascript.NodePackageManager.NPM,
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/shes.lora.reames/aws-cdk-tag-policy-aspect.git',
+  repositoryUrl:
+    'https://github.com/shes.lora.reames/aws-cdk-tag-policy-aspect.git',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
